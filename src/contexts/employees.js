1f7014643358b58867c9d1447ef3a4c7cf56e3e5
Sergio Xalambrí <hello@sergiodxa.com>
2019-06-09 23:10:00 -0500

    Create employee context

---
+++ src/contexts/employees.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const EmployeesContext = createContext();
+
+export const Provider = EmployeesContext.Provider;
+export const Consumer = EmployeesContext.Consumer;
